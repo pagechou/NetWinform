093635a5e20184449344e650de39f5332e244c0d
YuquanZhou <ZhouYuQuan@aacoptics.com>
2021-07-10 16:59:15 +0800

    first commit

---
+++ WinFormApp/WinForms/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Core
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //加载组件
+            InitMoudles();
+            Application.Run(new MainForm());
+        }
+
+        static void InitMoudles()
+        {
+
+        }
+    }
+}
